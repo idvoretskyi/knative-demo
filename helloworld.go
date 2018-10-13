79df7aa7a935dd2ad952301703b52e0649b37f92
Ihor Dvoretskyi <ihor@linux.com>
2018-10-13 07:45:39 +0000

    Dockerfile & helloworld.go added
    Signed-off-by: Ihor Dvoretskyi <ihor@linux.com>

---
+++ helloworld.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Hello world received a request.")
+	target := os.Getenv("TARGET")
+	if target == "" {
+		target = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!\n", target)
+}
+
+func main() {
+	flag.Parse()
+	log.Print("Hello world sample started.")
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
